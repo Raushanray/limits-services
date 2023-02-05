5da09bb2d18ebacf24da2dcefc0f496b4b3831fc
Raushanray <raushankuray1998@gmail.com>
2023-02-05 09:17:41 +0530

    Enhance limits service - Get configuration from application props - V2

--- src/main/java/com/codewithraushan/microservices/limitsservices/controller/LimitsController.java
+++ src/main/java/com/codewithraushan/microservices/limitsservices/controller/LimitsController.java
@@ -1,14 +1,20 @@
 package com.codewithraushan.microservices.limitsservices.controller;
 
 import com.codewithraushan.microservices.limitsservices.bean.Limits;
+import com.codewithraushan.microservices.limitsservices.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
 
+    @Autowired
+    private Configuration configuration;
+
     @GetMapping("/limits")
-    public Limits retrievelimits(){
-        return new Limits(1,1000);
+    public Limits retrievelimits() {
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
+//        return new Limits(1,1000);
     }
 }
